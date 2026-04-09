d73e2757e480a42e2c42e6313b1a34afca4b81d2
Kishan Kumar Gautam <67418820+Gkishan5499@users.noreply.github.com>
2026-04-10 00:40:03 +0530

    fix: bind server to 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,6 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 
 const PORT = Number(process.env.PORT) || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
 })
